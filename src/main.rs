e226af6a928b3d12a1d3b503483df53671dbfd0a
Niclas van Eyk <niclas.eyk@gmail.com>
2023-04-07 00:55:59 +0200

    table comparison

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
-use std::time::Instant;
-
+use cli_table::{format::Justify, print_stdout, Cell, Style, Table};
 use humantime::format_duration;
 use sequencer::builders::vector_with_holes;
+use std::{fmt::Alignment, time::Instant};
+
+struct Row {
+    length: usize,
+    took_ss: String,
+    took_cc: String,
+    comparisons_ss: u32,
+    comparisons_cc: u32,
+}
 
 pub fn main() {
     let ten: usize = 10;
+
+    let mut rows: Vec<Row> = Vec::new();
 
     for power in 1..10 {
         let length = ten.pow(power);
@@ -13,26 +23,46 @@ pub fn main() {
 
         println!("Running 'divide_and_conquer':");
         let now = Instant::now();
-        let (comparisons, number) = sequencer::divide_and_conquer::next_number(sequence.as_slice());
+        let (comparisons_cc, _) = sequencer::divide_and_conquer::next_number(sequence.as_slice());
         let elapsed = now.elapsed();
-        println!("next number is {}", number);
-        println!(
-            "took {} comparisons and ran in {:?}",
-            comparisons,
-            format_duration(elapsed).to_string()
-        );
-        println!("");
+        let took_cc = format_duration(elapsed).to_string();
 
         println!("Running 'sequential_search':");
         let now = Instant::now();
-        let (comparisons, number) = sequencer::sequential_search::next_number(sequence.as_slice());
+        let (comparisons_ss, _) = sequencer::sequential_search::next_number(sequence.as_slice());
         let elapsed = now.elapsed();
-        println!("next number is {}", number);
-        println!(
-            "took {} comparisons and ran in {:?}",
-            comparisons,
-            format_duration(elapsed).to_string()
-        );
-        println!("===============");
+        let took_ss = format_duration(elapsed).to_string();
+
+        rows.push(Row {
+            length,
+            took_ss,
+            took_cc,
+            comparisons_ss,
+            comparisons_cc,
+        });
+        println!("");
     }
+
+    let table = rows
+        .iter()
+        .map(|row| {
+            vec![
+                row.length.cell().justify(Justify::Right),
+                row.took_cc.clone().cell().justify(Justify::Right),
+                row.comparisons_cc.cell().justify(Justify::Right),
+                row.took_ss.clone().cell().justify(Justify::Right),
+                row.comparisons_ss.cell().justify(Justify::Right),
+            ]
+        })
+        .table()
+        .title(vec![
+            "Length".cell().bold(true),
+            "Took CC".cell().bold(true),
+            "Comparisons CC".cell().bold(true),
+            "Took SS".cell().bold(true),
+            "Comparisons SS".cell().bold(true),
+        ])
+        .bold(true);
+
+    assert!(print_stdout(table).is_ok());
 }
